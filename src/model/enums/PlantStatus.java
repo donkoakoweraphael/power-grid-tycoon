5fd0426de30644b20b605bed774866110ee4bf82
Akowé Raphaël DONKO <dar11022054@gmail.com>
2026-01-12 18:27:12 +0000

    feat: move and update enumerations to model.enums

--- src/model/enums/PlantStatus.java
+++ src/model/enums/PlantStatus.java
@@ -1,4 +1,4 @@
-package model.entity;
+package model.enums;
 
 /**
  * Enum representing the operational status of a power plant.
@@ -8,17 +8,17 @@ public enum PlantStatus {
      * The plant is currently under construction.
      */
     UNDER_CONSTRUCTION,
-    
+
     /**
      * The plant is currently being upgraded.
      */
     UPGRADING,
-    
+
     /**
      * The plant is active and operational.
      */
     ACTIVE,
-    
+
     /**
      * The plant is inactive (manually stopped or due to failure).
      */
